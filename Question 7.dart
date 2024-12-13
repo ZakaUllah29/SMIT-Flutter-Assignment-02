c25357dbe605eadb264680dbb81dbe365c9d0047
ZakaUllah29 <zakaullah1421@gmail.com>
2024-12-13 16:50:17 +0500

    Update Question 7.dart

--- Question 7.dart
+++ Question 7.dart
@@ -1,18 +1,7 @@
 void main() {
-  // Original expenses map
-  Map<String, double> expenses = {
-    'sun': 3000.0,
-    'mon': 3000.0,
-    'tue': 3234.0,
-  };
+  Map<String, double> expenses = {'sun': 3000.0, 'mon': 3000.0, 'tue': 3234.0};
+  print(expenses.containsKey("fri"));
 
-  // Check if 'fri' exists, and if so, update its value, otherwise add it
-  if (expenses.containsKey('fri')) {
-    expenses['fri'] = 5000.0;
-  } else {
-    expenses['fri'] = 5000.0;
-  }
-
-  // Print the updated expenses map
+  expenses.putIfAbsent("fri", () => 5000.0);
   print(expenses);
 }
