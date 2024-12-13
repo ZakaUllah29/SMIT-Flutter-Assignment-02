9c008d5b71b86727ee2f4ed0b1fa0ac7b1f17df2
ZakaUllah29 <zakaullah1421@gmail.com>
2024-12-13 16:51:20 +0500

    Update Question 9.dart

--- Question 9.dart
+++ Question 9.dart
@@ -1,15 +1,5 @@
 void main() {
-  // Original list of integers
-  List<int> numbers = [10, 20, 5, 40, 30, 60, 25];
-
-  // Call the function to get the maximum value
-  int maxValue = getMaxValue(numbers);
-
-  // Print the maximum value
-  print('The maximum value in the list is: $maxValue');
-}
-
-// Function to return the maximum value from the list
-int getMaxValue(List<int> numbers) {
-  return numbers.reduce((a, b) => a > b ? a : b);
+  List nums = [1, 99, 5, 47, 6, 23, 5];
+  var num1 = nums.reduce((a, b) => a > b ? a : b);
+  print("Maximum Value is : s$num1");
 }
