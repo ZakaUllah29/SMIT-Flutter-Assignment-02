bb09bc21e1330ce3fab1f5901ebc476c89d395dd
ZakaUllah29 <zakaullah1421@gmail.com>
2024-12-13 16:48:49 +0500

    Update Question 16.dart

--- Question 16.dart
+++ Question 16.dart
@@ -1,19 +1,5 @@
-// Q.16: Implement a Dart code that uses the where() method to filter out odd
-// numbers from a list of integers. The program should take in the original list as a
-// parameter and print a new list containing only the even numbers.
-
 void main() {
-  // Original list of integers
-  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-  // Call the function to filter out odd numbers and get the new list of even numbers
-  List<int> evenNumbers = filterEvenNumbers(numbers);
-
-  // Print the new list containing only even numbers
-  print(evenNumbers);
-}
-
-// Function that filters out odd numbers and returns only even numbers
-List<int> filterEvenNumbers(List<int> numbers) {
-  return numbers.where((number) => number % 2 == 0).toList();
+  List nums = [2, 9, 7, 27, 22, 4, 8, 11, 17, 10, 26];
+  var newlist = nums.where((x) => x % 2 == 0);
+  print(newlist);
 }
