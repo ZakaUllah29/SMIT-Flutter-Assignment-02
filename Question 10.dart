bfa80be967d66b44087ac58bebfd7732cca8cf32
ZakaUllah29 <zakaullah1421@gmail.com>
2024-12-13 20:35:18 +0500

    Update Question 10.dart

--- Question 10.dart
+++ Question 10.dart
@@ -1,25 +1,12 @@
 void main() {
-  // Original list of strings
-  List<String> words = ['apple', 'banana', 'apple', 'cherry', 'banana', 'date'];
-
-  // Call the function to get the list with unique elements
-  List<String> uniqueWords = removeDuplicates(words);
-
-  // Print the new list without duplicates
-  print(uniqueWords);
-}
-
-// Function to remove duplicates and return a new list
-List<String> removeDuplicates(List<String> words) {
-  List<String> result = [];
-  Set<String> seen = Set();
-
-  for (var word in words) {
-    if (!seen.contains(word)) {
-      result.add(word);
-      seen.add(word);
-    }
-  }
-
-  return result;
+  List<String> fruits = [
+    "Apple",
+    "Banana",
+    "Mango",
+    "Watermelon",
+    "Mango",
+    "Apple"
+  ];
+  Set unique = fruits.toSet();
+  print(unique);
 }
